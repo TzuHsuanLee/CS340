1afa9d23c6abcd710611d5a60b23472b68d16edb
TzuHsuanLee <leetz@oregonstate.edu>
2024-03-01 15:26:22 -0800

    Add hbs files in the views files

--- app.js
+++ app.js
@@ -19,39 +19,46 @@ var db = require('./database/db-connector')
 /*
     ROUTES
 */
+
+app.get('/', function(req, res)
+    {
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });    
+
+    
 // app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
 //     {
 //         res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
 //     });                                         // requesting the web site.
-app.get('/', function(req, res)
-    {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
+// app.get('/', function(req, res)
+//     {
+//         // Define our queries
+//         query1 = 'DROP TABLE IF EXISTS diagnostic;';
+//         query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+//         query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+//         query4 = 'SELECT * FROM diagnostic;';
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+//         // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
 
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
+//         // DROP TABLE...
+//         db.pool.query(query1, function (err, results, fields){
 
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
+//             // CREATE TABLE...
+//             db.pool.query(query2, function(err, results, fields){
 
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
+//                 // INSERT INTO...
+//                 db.pool.query(query3, function(err, results, fields){
 
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
+//                     // SELECT *...
+//                     db.pool.query(query4, function(err, results, fields){
 
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+//                         // Send the results to the browser
+//                         res.send(JSON.stringify(results));
+//                     });
+//                 });
+//             });
+//         });
+//     });
 
 /*
     LISTENER
