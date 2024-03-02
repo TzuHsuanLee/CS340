e8dddcf312ba7e81b2f716622741bd39b4788147
TzuHsuanLee <leetz@oregonstate.edu>
2024-03-01 18:38:22 -0800

    Let book page connect with home page

--- app.js
+++ app.js
@@ -5,14 +5,14 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 9044;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 9099;                 // Set a port number at the top so it's easy to change in the future
 
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
-
+app.use(express.static('view'))
 // Database
 var db = require('./database/db-connector')
 
