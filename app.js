272642071bcdccbfe7de09e99928f08bd7dce4da
TzuHsuanLee <leetz@oregonstate.edu>
2024-03-07 20:02:25 -0800

    finish delete function

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express');   // We are using the express library for the web server
 var bodyParser = require('body-parser');
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 9535;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 9534;                 // Set a port number at the top so it's easy to change in the future
 
 // create application/json parser
 var jsonParser = bodyParser.json()
@@ -73,6 +73,7 @@ app.get('/orderDetails', function(req, res)
     });                        // Note the call to render() and not send(). Using render() ensures the templating engine
 }); 
 
+//Delete
 app.delete('/delete-orderDetail-ajax/', jsonParser, function(req,res,next){
     let data = req.body;
     let bookID = parseInt(data.bookID);
@@ -95,6 +96,117 @@ app.delete('/delete-orderDetail-ajax/', jsonParser, function(req,res,next){
     
 });
 
+app.delete('/delete-book-ajax/', jsonParser, function(req,res,next){
+    let data = req.body;
+    let bookID = parseInt(data.bookID);
+    let deleteOrderDetail= `DELETE FROM OrderDetails WHERE bookID = ?`;
+    let deletebook= `DELETE FROM Books WHERE bookID = ?`;
+
+    // Run the 1st query
+    db.pool.query(deleteOrderDetail, [bookID], function(error, rows, fields){
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+            }
+
+            else
+            {
+                // Run the second query
+                db.pool.query(deletebook, [bookID], function(error, rows, fields) {
+
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                })
+            }
+    })
+});
+
+app.delete('/delete-order-ajax/', jsonParser, function(req,res,next){
+    let data = req.body;
+    let orderID = parseInt(data.orderID);
+    let deleteOrderDetail= `DELETE FROM OrderDetails WHERE orderID = ?`;
+    let deleteOrder= `DELETE FROM Orders WHERE orderID = ?`;
+
+    // Run the 1st query
+    db.pool.query(deleteOrderDetail, [orderID], function(error, rows, fields){
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+            }
+
+            else
+            {
+                // Run the second query
+                db.pool.query(deleteOrder, [orderID], function(error, rows, fields) {
+
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                })
+            }
+    })
+});
+
+app.delete('/delete-customer-ajax/', jsonParser, function(req,res,next){
+    let data = req.body;
+    let customerID = parseInt(data.customerID);
+    let findOrderDetails = `SELECT * FROM Orders WHERE customerID = ?`;
+    // let deleteOrderDetail= `DELETE FROM OrderDetails WHERE customerID = ?`;
+    // let deleteOrder= `DELETE FROM Orders WHERE customerID = ?`;
+    // let deleteCustomer= `DELETE FROM Customers WHERE customerID = ?`;
+
+    // Run the 1st query
+    db.pool.query(findOrderDetails, [customerID], function(error, rows, fields) {
+        rows.forEach((row) => {
+            console.log(row.orderID)
+        })
+    });
+
+    // db.pool.query(deleteOrderDetail, [customerID], function(error, rows, fields){
+    //     if (error) {
+
+    //         // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+    //         console.log(error);
+    //         res.sendStatus(400);
+    //         }
+
+    //         else
+    //         {
+    //             db.pool.query(deleteOrder, [customerID], function(error, rows, fields){
+    //             if (error) {
+
+    //                 // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+    //                 console.log(error);
+    //                 res.sendStatus(400);
+    //                 }
+
+    //                 else{
+    //                     // Run the second query
+    //                     db.pool.query(deleteCustomer, [customerID], function(error, rows, fields) {
+
+    //                         if (error) {
+    //                             console.log(error);
+    //                             res.sendStatus(400);
+    //                         } else {
+    //                             res.sendStatus(204);
+    //                         }
+    //                     })
+    //                 }
+    //             })
+    //         }
+    //     })
+});
 
 /*
     LISTENER
--- public/delete.js
+++ public/delete.js
@@ -1,23 +1,33 @@
-// // code for deletePerson function using jQuery
 function deleteBook(bookID) {
     console.log(bookID)
     let link = '/delete-book-ajax/';
     let data = {
-      id: bookID
+      bookID
     };
   
-    $.ajax({
-      url: link,
-      type: 'DELETE',
-      data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8", 
-      success: function(result) {
-        deleteRow(bookID);
-      }
-    });
-  }
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/delete-book-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+            // Add the new data to the table
+            // deleteRow(personID);
+            location.reload();
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+};
   
-  function deleteOrderDetail(orderID, bookID) {
+function deleteOrderDetail(orderID, bookID) {
     console.log(orderID, bookID);
     let link = '/delete-orderDetail-ajax/';
     let data = {
@@ -45,3 +55,62 @@ function deleteBook(bookID) {
     // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
 };
+
+function deleteOrder(orderID) {
+  console.log(orderID);
+  let link = '/delete-order-ajax/';
+  let data = {
+    orderID
+  };
+
+  // Setup our AJAX request
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("DELETE", "/delete-order-ajax", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  // Tell our AJAX request how to resolve
+  xhttp.onreadystatechange = () => {
+      if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+          // Add the new data to the table
+          // deleteRow(personID);
+          location.reload();
+
+      }
+      else if (xhttp.readyState == 4 && xhttp.status != 204) {
+          console.log("There was an error with the input.")
+      }
+  }
+  // Send the request and wait for the response
+  xhttp.send(JSON.stringify(data));
+};
+
+
+function deleteCustomer(customerID) {
+  console.log(customerID)
+  let link = '/delete-customer-ajax/';
+  let data = {
+    customerID
+  };
+
+  // Setup our AJAX request
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("DELETE", "/delete-customer-ajax", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  // Tell our AJAX request how to resolve
+  xhttp.onreadystatechange = () => {
+      if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+          // Add the new data to the table
+          // deleteRow(personID);
+          location.reload();
+
+      }
+      else if (xhttp.readyState == 4 && xhttp.status != 204) {
+          console.log("There was an error with the input.")
+      }
+  }
+  // Send the request and wait for the response
+  xhttp.send(JSON.stringify(data));
+}
